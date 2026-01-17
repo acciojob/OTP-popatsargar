1f6dd0a110666b7a8adf3515db372566a9b12048
Popat Bhausaheb Sargar <sargarpopat219@gmail.com>
2026-01-18 01:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+  
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+	input.addEventListener('keydown', (e) => {
+
+		 
+		if (e.key === 'Backspace') {
+			if (input.value === '' && index > 0) {
+				inputs[index - 1].value = '';
+				inputs[index - 1].focus();
+			} else {
+				input.value = '';
+			}
+		}
+ 
+		if (e.key >= '0' && e.key <= '9') {
+			input.value = '';
+			setTimeout(() => {
+				if (index < inputs.length - 1) {
+					inputs[index + 1].focus();
+				}
+			}, 10);
+		}
+	});
+});
